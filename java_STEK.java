dc43faf24b399eefb8251b9f138e3823942e2e2b
giga-nija <bmyshoe097@gmail.com>
2025-09-18 00:51:34 +0300

    Create java_STEK.java

---
+++ java_STEK.java
@@ -0,0 +1,14 @@
+import java.util.Stack;
+
+public class StackExample {
+    public static void main(String[] args) {
+        // Создаем стек для хранения целых чисел
+        Stack<Integer> stack = new Stack<>();
+        
+        // Добавляем элементы в стек (push)
+        stack.push(10);  // Положили 10 на вершину
+        stack.push(20);  // Положили 20 на вершину
+        stack.push(30);  // Положили 30 на вершину
+        }
+    }
+}
