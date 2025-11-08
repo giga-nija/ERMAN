2d4c8eb22bee06d3d2b792284591a13b08724c2c
giga-nija <bmyshoe097@gmail.com>
2025-11-08 14:12:16 +0300

    Create java_code.java

---
+++ РЕКУРС.КОМБ./java_code.java
@@ -0,0 +1,69 @@
+public class SudokuSolver {
+    private static final int SIZE = 9;
+
+    public static void main(String[] args) {
+        int[][] board = {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        if (solve(board)) {
+            printBoard(board);
+        } else {
+            System.out.println("No solution exists");
+        }
+    }
+
+    public static boolean solve(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                if (board[row][col] == 0) {
+                    for (int num = 1; num <= 9; num++) {
+                        if (isValid(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solve(board)) return true;
+                            board[row][col] = 0; // backtrack
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true; // solved
+    }
+
+    private static boolean isValid(int[][] board, int row, int col, int num) {
+        for (int i = 0; i < SIZE; i++) {
+            if (board[row][i] == num  board[i][col] == num 
+                board[3*(row/3) + i/3][3*(col/3) + i%3] == num)
+                return false;
+        }
+        return true;
+    }
+
+    private static void printBoard(int[][] board) {
+        for (int r = 0; r < SIZE; r++) {
+            for (int c = 0; c < SIZE; c++)
+                System.out.print(board[r][c] + " ");
+            System.out.println();
+        }
+    }
+}
+Пример ввода 
+
+5 3 4 6 7 8 9 1 2 
+6 7 2 1 9 5 3 4 8 
+1 9 8 3 4 2 5 6 7 
+8 5 9 7 6 1 4 2 3 
+4 2 6 8 5 3 7 9 1 
+7 1 3 9 2 4 8 5 6 
+9 6 1 5 3 7 2 8 4 
+2 8 7 4 1 9 6 3 5 
+3 4 5 2 8 6 1 7 9
